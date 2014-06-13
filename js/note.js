0987dfa02a29d18dfa05f60282f76355684c6f15
FrancisMurillo <francisavmurillo@gmail.com>
2014-06-13 08:36:24 +0800

    Refactor note creation process
    Refactor note creation logic to note.js

---
+++ js/note.js
@@ -0,0 +1,41 @@
+/* Create a note */
+function createNewNote(width , height , target_elem , note_class) {	
+	var noteHTML = "<div></div>";
+	var $newNote = $(noteHTML, {
+						'class'	:	note_class,
+						'text'	:	'Hello'			
+					});
+
+	$newNote.appendTo($(target_elem));	
+	$newNote.draggable({
+		containment:'parent' , 
+		zIndex : 9007199254740992,	
+		stop: function() {
+			$(this).css('z-index' , nextZIndex(note_class));
+		}
+	})
+	$newNote.resizable();
+					
+	$newNote.css({			
+		'width' 	: 	toPx(width) 	, 
+		'height'	: 	toPx(height)	,
+		
+		'left'		:	toPx(randomNoteX(width , $('body').width()))	,
+		'top'		:	toPx(randomNoteY(height , $('body').height()))	,
+		
+		'zIndex'	:	nextZIndex(note_class)
+	});
+}
+
+function nextZIndex(note_class) {
+	return $.topZIndex('.' + note_class) + 1;
+}
+
+function randomNoteX(width , containerWidth) {
+	return	( Math.random() * (containerWidth - 2 *  width)).toFixed();
+}
+
+/* Generates a random Y coordinate for a note, see randomNoteX() */ 	
+function randomNoteY(height , containerHeight) {
+	return ( Math.random() * (containerHeight - 2 *  height)).toFixed();
+}
